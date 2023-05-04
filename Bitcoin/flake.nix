9df3ed13aa55e696f45b5eec22a629d4a2cdebfb
edwardhodl <edwardhodl@protonmail.com>
2023-05-04 11:16:46 -0700

    Replace genericBuild with custom placeholder phases.

--- Bitcoin/flake.nix
+++ Bitcoin/flake.nix
@@ -15,6 +15,59 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages.bitcoin = pkgs.stdenv.mkDerivation rec {
+          pname = "bitcoin";
+          version = "0.1.0";
+
+          # (Custom) Unpack Phase
+          unpackPhase = ''
+            runHook preUnpack
+            echo "Running custom unpack phase"
+            runHook postUnpack
+          '';
+
+          # (Custom) Configure Phase
+          configurePhase = ''
+            runHook preConfigure
+            echo "Running custom configure phase"
+            runHook postConfigure
+          '';
+
+          # (Custom) Build Phase
+          buildPhase = ''
+            runHook preBuild
+            echo "Running custom build phase"
+            runHook postBuild
+          '';
+
+          # (Custom) Check Phase
+          checkPhase = ''
+            runHook preCheck
+            echo "Running custom check phase"
+            runHook postCheck
+          '';
+
+          # (Custom) Install Phase
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            echo "Placeholder" > $out/placeholder.txt
+            runHook postInstall
+          '';
+
+          # Disable each standard-environment, 'genericBuild' phase.
+          #  https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases
+          dontUnpack = false;           # unpack
+          dontPatch = true;
+          dontConfigure = false;	# configure
+          dontBuild = false;		# build
+          doCheck = true;		# check
+          dontInstall = false;		# install
+          dontFixup = true;
+          doInstallCheck = false;
+          dontCopyDist = true;
+        };
+
         devShells.default = import ./shell.nix {inherit pkgs; };
       }
    );
