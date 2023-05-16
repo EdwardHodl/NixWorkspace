fc0b1e609c036c2d157ab01dad8f6923e7a309e2
edwardhodl <edwardhodl@protonmail.com>
2023-05-16 18:46:16 +0000

    Fixup Nostr/shell.nix shellhook echos

--- Nostr/shell.nix
+++ Nostr/shell.nix
@@ -13,9 +13,10 @@ stdenv.mkDerivation {
     cat README.md
 
     export PRIMALSERVER_HOST="0.0.0.0"
-    echo "set PRIMALSERVER_HOST to 0.0.0.0 (bind to all LAN)"
+    echo "set PRIMALSERVER_HOST to $PRIMALSERVER_HOST (bind to all LAN)"
 
     export PRIMALSERVER_STORAGE_PATH="/home/edward/Nostr/data"
+    echo "set PRIMALSERVER_STORAGE_PATH to $PRIMALSERVER_STORAGE_PATH"
 
     alias primal="nix develop -c sh -c '\$start_primal_caching_service'"
     alias
