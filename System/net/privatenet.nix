7c85e08c2e29d55a950257d6f96462ba2084d054
edwardhodl <edwardhodl@protonmail.com>
2023-05-26 05:46:02 +0000

    Add mullvad configuration to privatenet.service

--- System/net/privatenet.nix
+++ System/net/privatenet.nix
@@ -5,21 +5,49 @@
       ''
       #!/bin/sh
       NAME="privatenet.service"
-      echo "$NAME: ### Starting ###" | systemd-cat -p info
+      echo "$NAME: ### Starting ###"
 
+      echo "Account and Key"
+      ACCOUNT=$(mullvad account get)
+      # if not logged in, then login
+      # also, logout on service stop
+      if echo "$ACCOUNT" | grep -q "Not logged in on any account"; then
+        echo "$ACCOUNT"
+        exit 1
+      else
+        echo "$ACCOUNT" | grep -A 1 "Device name"
+      fi
+
+      #mullvad tunnel wireguard key check
+
+      echo "Configure Interface"
+      mullvad auto-connect set on
+      mullvad lan set allow
+      mullvad dns set default --block-ads --block-malware --block-trackers --block-gambling --block-adult-content
+      mullvad obfuscation set mode auto
+
+      echo "Configure Relay and Tunnel"
+      mullvad relay set tunnel-protocol wireguard
+      mullvad relay set location us
+      mullvad tunnel wireguard quantum-resistant-tunnel set off
+
+      echo "Check Status and Always Connect"
       while :
       do
         STATUS=$(mullvad status)
-        echo $STATUS | systemd-cat -p info
+        echo $STATUS
         if echo "$STATUS" | grep -q "Connected"; then
-          echo "Mullvad is connected"
+          sleep 5
         else
           echo "Mullvad is not connected, connecting..."
           mullvad connect
+          sleep 2
         fi
-
-        sleep 2
       done
+
+      # Update Commands.
+      # mullvad tunnel wireguard key regenerate
+      # mullvad relay update
       '';
   in
 {
