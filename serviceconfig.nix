30d161937054f3879bc4cc70006977c6d01e2af6
edwardhodl <edwardhodl@protonmail.com>
2023-06-02 20:56:07 +0000

    Add and Enable PhotoPrism service

--- serviceconfig.nix
+++ serviceconfig.nix
@@ -4,6 +4,22 @@
    imports = [
     # Mempool
     ./MySQL/configuration.nix
+
     ./Nextcloud.nix
+    #./PhotoPrism.nix
    ];
+
+
+   services.photoprism = {
+    enable = true;
+    package = pkgs.photoprism;
+    #storagePath = /home/edward/NixWorkspace/PhotoPrism/data;
+    address = "0.0.0.0";
+    port = 2342; # default 2342
+    originalsPath = /var/lib/nextcloud/data;
+    settings = {
+      PHOTOPRISM_ADMIN_USER = "root";
+      PHOTOPRISM_DEFAULT_LOCALE = "en";
+    };
+  };
 }
